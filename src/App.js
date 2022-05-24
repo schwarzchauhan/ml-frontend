17415739efdf49d3e0c85a47806dae506aa51ca8
harsh chauhan pc2 <harshchauhan0994@gmail.com>
2022-05-24 14:53:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './assets/styles/sass/App.scss';
+
+function App() {
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="logo">
+            <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="" />
+        </div>
+        <div className="text-center mt-4 name">
+            Twitter
+        </div>
+        <form className="p-3 mt-3">
+            <div className="form-field d-flex align-items-center">
+                <span className="far fa-user"></span>
+                <input type="text" name="userName" id="userName" placeholder="Username" />
+            </div>
+            <div className="form-field d-flex align-items-center">
+                <span className="fas fa-key"></span>
+                <input type="password" name="password" id="pwd" placeholder="Password" />
+            </div>
+            <button className="btn mt-3">Login</button>
+        </form>
+        <div className="text-center fs-6">
+            <a href="#">Forget password?</a> or <a href="#">Sign up</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
